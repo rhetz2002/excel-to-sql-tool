23315307cbb0606a5340d2cac1da629e0b957d31
rhetz2002 <rhetzyorki002@gmail.com>
2026-07-21 09:14:30 -0600

    added functunality to append like ids with matching names

--- tool/validation.py
+++ tool/validation.py
@@ -209,10 +209,9 @@ def validate(filepath, data):
 
     id_check = [row[0] for row in db] 
     
-    db.execute("PRAGMA table_info(employee_sales)")
-    column_names = [column[1] for column in db.fetchall()]
-    print (column_names)
-    
+    db.execute("SELECT employee_name FROM employee_sales")
+
+    name_check = [row[0].strip() for row in db]
 
     succsess = 0
 
@@ -220,7 +219,7 @@ def validate(filepath, data):
 
     # iterates through the data taken from excell file
     # loops for the index and row
-    bad_rows = []
+    omit_rows = []
 
     print("rows processed")
     print('--------------------------------------------')
@@ -230,154 +229,170 @@ def validate(filepath, data):
     
     print (data)
     for index, row in data.iterrows():
+        
+        check = 0
 
-            check = 0
-
-            # thuroughly checks to see if its missing any
-            # important data or formated incorectly
+        # thuroughly checks to see if its missing any
+        # important data or formated incorectly
             
-            # checks if ID collomn is not empty
-            # if it is not valid sets check to one to set the error
+        # checks if ID collomn is not empty
+        # if it is not valid sets check to one to set the error
 
-            if pd.notna(row['ID']):
-                
-                # checks if name collomn is not empty, dose not contain a digit, 
-                # is longer than 2 charicters long, and has a space
-                # if it is not valid sets check to two to set the error
+        if pd.notna(row['ID']):
+            
+            # checks if name collomn is not empty, dose not contain a digit, 
+            # is longer than 2 charicters long, and has a space
+            # if it is not valid sets check to two to set the error
 
-                if pd.notna(row['name']) and not any(char.isdigit() for char in str(row['name'])) and len(str(row['name'])) >= 3 and ' ' in str(row['name']):
+            if pd.notna(row['name']) and not any(char.isdigit() for char in str(row['name'])) and len(str(row['name'])) >= 3 and ' ' in str(row['name']):
 
-                    # checks if email collomn is not empty, contains an @ symbol as well as .com, 
-                    # and is longer than nine charicters
-                    # if it is not valid sets check to three to set the error
+                # checks if email collomn is not empty, contains an @ symbol as well as .com, 
+                # and is longer than nine charicters
+                # if it is not valid sets check to three to set the error
 
-                    if pd.notna(row['email']) and '@' in str(row['email']) and '.com' in str(row['email']) and len(str(row['email'])) >= 9:
+                if pd.notna(row['email']) and '@' in str(row['email']) and '.com' in str(row['email']) and len(str(row['email'])) >= 9:
                         
-                        try: 
+                    try: 
 
-                            # checks that sales colounm is float
+                        # checks that sales colounm is float
 
-                            float(row['sales'])
+                        float(row['sales'])
                         
-                        except: 
+                    except: 
 
-                            # if it is not a float sets check to six to set the error
+                        # if it is not a float sets check to six to set the error
 
-                            check = 6    
+                        check = 6    
 
-                        # checks that department has a valid string
+                    # checks that department has a valid string
 
-                        if pd.isna(row['department']) or str(row['department']).strip().lower() in ['sales', 'customer service', '']:
+                    if pd.isna(row['department']) or str(row['department']).strip().lower() in ['sales', 'customer service', '']:
 
 
-                            try:
+                        try:
                                 
                                 
-                                # checks that ID colomn is an intiger
+                            # checks that ID colomn is an intiger
 
-                                if int(row['ID']) not in id_check:
+                            if int(row['ID']) not in id_check:
                                 
-                                    # adds one to succsess to keep track of valid rows
+                                # adds one to succsess to keep track of valid rows
                             
-                                    succsess = succsess + 1                                
+                                succsess = succsess + 1                                
                                 
-                                else:
+                            elif str(row['name']).strip() in name_check:
                                     
-                                    check = 4
-                                
-                            except:
+                                db.execute("""
+                                    UPDATE employee_sales
+                                    SET employee_email = ?,
+                                        department = ?,
+                                        sales = ?
+                                    WHERE employee_ID = ?""",
+                                    (str(row['email']), str(row['department']).strip(), float(row['sales']), int(row['ID'])))
 
-                                # if it is not an intiger sets check to four to set the error
+                                omit_rows.append(index)
+                                
+                                # adds one to succsess to keep track of valid rows
+                            
+                                succsess += 1     
+                                    
+                            else:
 
                                 check = 4
 
-                        else:
+                        except:
+
+                            # if it is not an intiger sets check to four to set the error
+
+                            check = 4
+
+                    else:
                             
-                            # if it is not valid sets check to eight to set the error
+                        # if it is not valid sets check to eight to set the error
                             
-                            check = 8
+                        check = 8
                 
                         
 
-                    else:
-
-                        # if is missing, or is missing @, or missing 
-                        # .com, or is les than 9 charicters long
-                        # sets check to 3 to set up for error mesage
-
-                        check = 3 
-
                 else:
-                
-                    # if is missing or is less than 3 charicters  
-                    # long or has digit or missing a space
-                    # sets check to 2 to set up for error mesage       
 
-                    check = 2                            
+                    # if is missing, or is missing @, or missing 
+                    # .com, or is les than 9 charicters long
+                    # sets check to 3 to set up for error mesage
+
+                    check = 3 
 
             else:
+                
+                # if is missing or is less than 3 charicters  
+                # long or has digit or missing a space
+                # sets check to 2 to set up for error mesage       
 
-                # if missing sets check to 1 to set up for error message
+                check = 2                            
 
-                check = 1
+        else:
 
-            # checks if line is bad and which line is bad using 
-            # the check variable                    
+            # if missing sets check to 1 to set up for error message
 
-            if check == 0:
+            check = 1
+
+        # checks if line is bad and which line is bad using 
+        # the check variable                    
+
+        if check == 0:
                 
                 # if no errors
                 # adds one to sucsess to keep track of number good lines
                 
-                print(f"row {index} sucsess") 
+            print(f"row {index} sucsess") 
 
-            else: 
+        else: 
 
-                #prints which row pass and fail
-                # adds 1 to fail to keep track of number of bad lines
+            #prints which row pass and fail
+            # adds 1 to fail to keep track of number of bad lines
                 
-                fail = fail + 1
-                print(f"row {index} fail")
+            fail = fail + 1
+            print(f"row {index} fail")
                 
-                # appends index of bad line to bad_rows list
+            # appends index of bad line to omit_rows list
                 
-                bad_rows.append(index)
+            omit_rows.append(index)
                 
-                # resets check variable
+            # resets check variable
                 
                 
 
-            # uses check to see what part of line failed to insert into
+        # uses check to see what part of line failed to insert into
             
-            if check == 1 or check == 4:
+        if check == 1 or check == 4:
 
-                log = log + f'{time.strftime("%I:%M:%S %p")}: row ' \
-                            f'{index} fail on ID check, pleas check ' \
-                            f'that ID is present, is int, or not duplicated\n'
+            log = log + f'{time.strftime("%I:%M:%S %p")}: row ' \
+                        f'{index} fail on ID check, pleas check ' \
+                        f'that ID is present, is int, or not duplicated\n'
                 
-            elif check == 2:
+        elif check == 2:
 
-                log = log + f'{time.strftime("%I:%M:%S %p")}: row ' \
-                            f'{index} fail on name check, pleas ' \
-                            f'check that name is present\n'
+            log = log + f'{time.strftime("%I:%M:%S %p")}: row ' \
+                        f'{index} fail on name check, pleas ' \
+                        f'check that name is present\n'
 
-            elif check == 3:
+        elif check == 3:
 
-                log = log + f'{time.strftime("%I:%M:%S %p")}: row {index}' \
-                            f' fail on email check, pleas check that email ' \
-                            f'is present and is in proper format\n'
+            log = log + f'{time.strftime("%I:%M:%S %p")}: row {index}' \
+                        f' fail on email check, pleas check that email ' \
+                        f'is present and is in proper format\n'
 
-            elif check == 6:
+        elif check == 6:
 
-                log = log + f'{time.strftime("%I:%M:%S %p")}: row {index}' \
-                            f' fail on sales check,' \
-                            f' pleas check that sales is int\n'
+            log = log + f'{time.strftime("%I:%M:%S %p")}: row {index}' \
+                        f' fail on sales check,' \
+                        f' pleas check that sales is int\n'
 
-            elif check == 8:
+        elif check == 8:
 
-                log = log + f'{time.strftime("%I:%M:%S %p")}: row {index} ' \
-                            f'fail on department check, pleas check that ' \
-                            f'feild contains vaild department\n'
+            log = log + f'{time.strftime("%I:%M:%S %p")}: row {index} ' \
+                        f'fail on department check, pleas check that ' \
+                        f'feild contains vaild department\n'
 
 
     # prints pass fail mesage to terminal
@@ -391,7 +406,7 @@ def validate(filepath, data):
     # stores passing data in clean
     # while removing bad rows
 
-    clean = data.drop(bad_rows, axis=0)
+    clean = data.drop(omit_rows, axis=0)
 
     # adds passing and failing lines to log
 
