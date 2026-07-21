f2b9bff593d7e571b109eddb912ebb8b77073eef
rhetz2002 <rhetzyorki002@gmail.com>
2026-07-21 12:10:50 -0600

    cleaned up input prompt and comment spelling and grammer, pending pep8 completion

--- tool/SQLcheck.py
+++ tool/SQLcheck.py
@@ -2,32 +2,32 @@ import time
 import sqlite3 as sq
 
 
-# defines vaildate function
+# defines SQL_check function
 def SQL_check(filepath):
 
     log = ''
 
-    # for while loop
-    # while loop will not end untill sql
+    # var for while loop
+    # while loop will not end until sql
     # file is present and db_open is set to true
 
     db_open = False
 
     # while loop while var is false indicating there is no sql file
-    # the loop will continue, will end when var is true
+    # the loop will continue, will end when var is set to true
     # indicating sql file is present
 
     while (db_open is False):
         
-        # trys to open file in file path
+        # tries to open file in file path
         
 
         try:
 
             # attempts to open file, purposefully made
-            # to fail if its not there as a qol feature
+            # to fail if its not there as a QoL feature
             # as a measure to make sure the user knows that
-            # its not typed in correctly, opens if alredy exsists
+            # its not typed in correctly, opens if already exists
             
             database = sq.connect(f"file:{filepath}?mode=rw", uri=True)
 
@@ -43,10 +43,6 @@ def SQL_check(filepath):
             db.execute('SELECT department FROM employee_sales')
             db.execute('SELECT sales FROM employee_sales')
                        
-
-            # if not, sets variable in order to have specail error mesage propting 
-            # user to re-enter or posibly injecting new table into db  
-
             # sets db_open to true to end loop
 
             db_open = True
@@ -59,16 +55,16 @@ def SQL_check(filepath):
                 database = sq.connect(f"file:{filepath}?mode=rw", uri=True)
 
 
-                if input ("there is a db here but the schema required dose not exist, do you want to add it? [Y/N] ").lower() == 'y':
+                if input ("there is a database here but the schema required dose not exist, do you want to add it? [Y/N] ").lower() == 'y':
                 
                     log = log + f'{time.strftime("%I:%M:%S %p")}: required db ' \
-                                f'schema not found in {filepath}, asking user for permision to create schema\n'
+                                f'schema not found in {filepath}, asking user for permission to create schema\n'
 
                     db = database.cursor()
                 
                     # creates table in db
                     
-                    log += 'atempting to create new table in exsisting db file'
+                    log += 'attempting to create new table in existing db file'
 
                     db.execute(
                                 """ CREATE TABLE IF NOT EXISTS employee_sales (
@@ -77,6 +73,7 @@ def SQL_check(filepath):
                                 employee_email text NOT NULL,
                                 department text,
                                 sales real)""")
+                    
                     database.commit()
                     
                     db.execute("PRAGMA table_info(employee_sales)")
@@ -93,7 +90,7 @@ def SQL_check(filepath):
                         
                         except:
                             
-                            log += 'appending to exsisting schema: failed to create employee_name'
+                            log += 'appending to existing schema: failed to create employee_name'
 
                     if 'employee_email' not in column_name:
 
@@ -105,7 +102,7 @@ def SQL_check(filepath):
                         
                         except:
                             
-                            log += 'appending to exsisting schema: failed to create employee_email'
+                            log += 'appending to existing schema: failed to create employee_email'
 
                     if 'department' not in column_name:
 
@@ -117,7 +114,7 @@ def SQL_check(filepath):
                         
                         except:
                             
-                            log += 'appending to exsisting schema: failed to create department'
+                            log += 'appending to existing schema: failed to create department'
 
                     if 'sales' not in column_name:
                         
@@ -129,14 +126,11 @@ def SQL_check(filepath):
                         
                         except:
                             
-                            log += 'appending to exsisting schema: failed to create sales'
-                    
-                             
-                    print (column_name)
+                            log += 'appending to existing schema: failed to create sales'
 
 
 
-                    log = log + f'{time.strftime("%I:%M:%S %p")}: sucseffully ' \
+                    log = log + f'{time.strftime("%I:%M:%S %p")}: successfully ' \
                                 f'added database schema in {filepath}\n'
                     
                     # sets db to true
@@ -146,7 +140,7 @@ def SQL_check(filepath):
 
             except:
 
-                if input('there is no valid file in this location '
+                if input('there is no valid database file in this location '
                       'do you want to create one?[Y/N]: ').lower() == 'y':
 
                     # creates file
@@ -158,18 +152,13 @@ def SQL_check(filepath):
                 
                     database = sq.connect(filepath)
 
-                    # initiates coursor as db
+                    # initiates cursor as db
 
                     db = database.cursor()
 
                     # executes SQL here to create the data tables
-
-                    # im sure that theres a way to create
-                    # the table depending on the contents
-                    # of the xlsx file but for the puroposes
-                    # of the asignment ill just hard code it
                 
-                    log = log + f'{time.strftime("%I:%M:%S %p")}: sucseffully ' \
+                    log = log + f'{time.strftime("%I:%M:%S %p")}: successfully ' \
                             f'created database in {filepath}\n'
 
                     # creates table in db
@@ -192,10 +181,12 @@ def SQL_check(filepath):
             if not db_open:
 
                 # if user selects no in last prompt,
-                # prompts user to input path to sql file again
+                # prompts user to input path to SQL file again
 
-                filepath = input('input path to database file, include file name: ')
-            
+                filepath = input('input path to database file, include file name and extension: ')
+
+    # commits the changes made if any then closes
+
     database.commit()
     database.close()            
     
